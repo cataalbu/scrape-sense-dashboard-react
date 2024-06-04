bdc6ced62d212b09d00b4b8191c1567ad53aebe2
Catalin Albu <cataalbu1919@gmail.com>
2024-06-04 11:27:37 +0300

    change time formating

--- src/utils/formatTime.ts
+++ src/utils/formatTime.ts
@@ -1,7 +1,7 @@
 import { format } from 'date-fns';
 
 export function fDate(date: Date) {
-  return format(date, 'dd/MM/yyyy HH:mm:ss');
+  return format(date, 'dd/MM/yyyy HH:mm:ss.SSS');
 }
 
 export function fStringDate(dateString?: string) {
@@ -11,6 +11,6 @@ export function fStringDate(dateString?: string) {
 export function fMinutesAndSeconds(startDate: Date, endDate: Date) {
   const diff = Math.abs(startDate.getTime() - endDate.getTime());
   const minutes = Math.floor(diff / 60000);
-  const seconds = (diff % 60000) / 1000;
-  return `${minutes}:${seconds < 10 ? '0' : ''}${seconds.toFixed(0)}`;
+  const seconds = ((diff % 60000) / 1000).toFixed(2);
+  return `${minutes}:${seconds.padStart(5, '0')}`;
 }
